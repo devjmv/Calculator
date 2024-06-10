bb1c265a8bfad369d29f371b5c5681e3958e64c9
eyadcode24 <eyadcode24@gmail.com>
2024-06-10 23:52:17 +0200

    operators first action

--- src/main.js
+++ src/main.js
@@ -4,3 +4,34 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const num1 = 3
+const num2 = 4 
+
+function calculator(num1, num2, operator){
+    let total;
+    if (operator === '+') 
+    total= num1 + num2
+    return total
+}
+function calculator(num1, num2, operator){
+    let total;
+    if (operator === '-') 
+    total= num1 - num2
+    return total
+}
+function calculator(num1, num2, operator){
+    let total;
+    if (operator === '*') 
+    total= num1 * num2
+    return total
+}
+function calculator(num1, num2, operator){
+    let total;
+    if (operator === '/') 
+    total= num1 / num2
+    return total
+}
+    
+    
+
