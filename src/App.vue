0cc70a9ab7a07691f66d02ea5f022da3c6443a41
devjmv <javier161890@gmail.com>
2024-06-17 08:48:56 +0200

    init calculator, time and convert component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,36 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TheCalc from './components/TheCalc.vue';
+import TheTime from './components/TheTime.vue';
+import TheConvert from './components/TheConvert.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="#clac">Calc</a>
+          <a class="nav-link" href="#time">Time</a>
+          <a class="nav-link" href="#convert">Convert</a>
+        </div>
+      </div>
     </div>
-  </header>
+  </nav>
 
-  <main>
-    <TheWelcome />
+  <main class="container mt-5">
+    <HelloWorld id="clac" class="pt-5" msg="Calculator" />
+    <TheCalc  class="mt-1"/>
+    <HelloWorld id="time" class="pt-5" msg="The Time" />
+    <TheTime  class="mt-5"/>
+    <HelloWorld id="convert" class="pt-5" msg="The Convertor" />
+    <TheConvert  class="mt-5"/>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/TheCalc.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="calculator card">
+
+      <input type="text" class="calculator-screen z-depth-1" value="" disabled />
+
+      <div class="calculator-keys">
+
+        <button type="button" class="operator btn btn-info" value="+">+</button>
+        <button type="button" class="operator btn btn-info" value="-">-</button>
+        <button type="button" class="operator btn btn-info" value="*">&times;</button>
+        <button type="button" class="operator btn btn-info" value="/">&divide;</button>
+
+        <button type="button" value="7" class="btn btn-light">7</button>
+        <button type="button" value="8" class="btn btn-light">8</button>
+        <button type="button" value="9" class="btn btn-light">9</button>
+
+
+        <button type="button" value="4" class="btn btn-light">4</button>
+        <button type="button" value="5" class="btn btn-light">5</button>
+        <button type="button" value="6" class="btn btn-light">6</button>
+
+
+        <button type="button" value="1" class="btn btn-light">1</button>
+        <button type="button" value="2" class="btn btn-light">2</button>
+        <button type="button" value="3" class="btn btn-light">3</button>
+
+
+        <button type="button" value="0" class="btn btn-light">0</button>
+        <button type="button" class="decimal function btn btn-secondary" value=".">.</button>
+        <button type="button" class="all-clear function btn btn-danger btn-sm" value="all-clear">AC</button>
+
+        <button type="button" class="equal-sign operator btn btn-default" value="=">=</button>
+
+        <button type="button" class="operator btn btn-info" value="MR">MR</button>
+        <button type="button" class="operator btn btn-info" value="M+">M+</button>
+        <button type="button" class="operator btn btn-info" value="MC">MC</button>
+
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+.calculator {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 400px;
+}
+
+.calculator-screen {
+  width: 100%;
+  height: 80px;
+  border: none;
+  background-color: #252525;
+  color: #fff;
+  text-align: right;
+  padding-right: 20px;
+  padding-left: 10px;
+  font-size: 4rem;
+}
+
+button {
+  height: 60px;
+  font-size: 2rem !important;
+}
+
+.equal-sign {
+  height: 98%;
+  grid-area: 2 / 4 / 6 / 5;
+}
+
+.calculator-keys {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  padding: 20px;
+}
+</style>
